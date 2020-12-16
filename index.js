c98abba2dcde073c68a821dcafc5353d191fb4c1
AgataLeal <agatharogowska@gmail.com>
2020-12-16 20:29:34 +0100

    started styling

--- index.js
+++ index.js
@@ -1,9 +1,42 @@
 function computerPlay() 
 {
-    const choices = ["Rock", "Paper", "Scissors"]
+    const choices = ["ROCK", "PAPER", "SCISSORS"]
     const random = Math.floor(Math.random() * choices.length);
     return choices[random]
 }
-let choice = computerPlay()
 
+function singleRound(player, computer)
+{
+    player = player.toUpperCase();
+    let result;
+    console.log("Player Choice:", player)
+    console.log("Computer Choice:", computer)
+    if (player === computer) {
+        result = "That's a tie!"
+    }
+    if (player === "SCISSORS") {
+        if (computer === "ROCK") {
+            result = "You Lose! Rock beats Scissors"
+        } else {
+            result = "You Win! Scissors beats Paper"
+        }
+    if (player === "PAPER") {
+        if (computer === "ROCK") {
+            result = "You Win! Paper beats Rock"
+        } else {
+            result = "You Lose! Scissors beats Paper"
+        }
+    if (player === "ROCK") {
+        if (computer === "PAPER") {
+            result = "You Lose! Paper beats Rock"
+        } else {
+            result = "You Win! Rock beats Scissors"
+        }
+    }
+    
+    return result;
+}
 
+let playerSelection = prompt('Please choose "Rock", "Paper" or "Scissors": ');
+
+let computerSelection = computerPlay()
