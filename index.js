31e32c8feddf00275cd2e0d15d44c71160694bf2
AgataLeal <agatharogowska@gmail.com>
2020-12-17 19:05:32 +0100

    made scoreboard functional

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 let round_result = document.querySelector('#round_result');
+let playerScoreSpan = document.querySelector('#player-score');
+let computerScoreSpan = document.querySelector('#computer-score');
+let playerScore = 0
+let computerScore = 0
 function computerPlay() 
 {
     const choices = ["ROCK", "PAPER", "SCISSORS"]
@@ -18,22 +22,31 @@ function singleRound(player)
     } else if (player === "SCISSORS") {
         if (computer === "ROCK") {
             result = "You Lose! Rock beats Scissors"
+            ++computerScore
         } else {
             result = "You Win! Scissors beats Paper"
+            ++playerScore
         }
     } else if (player === "PAPER") {
         if (computer === "ROCK") {
             result = "You Win! Paper beats Rock"
+            ++playerScore
         } else {
             result = "You Lose! Scissors beats Paper"
+            ++computerScore
         }
     } else if (player === "ROCK") {
         if (computer === "PAPER") {
             result = "You Lose! Paper beats Rock"
+            ++computerScore
         } else {
             result = "You Win! Rock beats Scissors"
+            ++playerScore
         }
     }
     console.log(result)
     round_result.textContent=result;
+    playerScoreSpan.textContent= playerScore;
+    computerScoreSpan.textContent= computerScore;
+    
 }
